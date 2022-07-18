719c745369ffb457ca616d3ed58857c6efb993fa
mochidamnit <corneliusang@outlook.com>
2022-07-19 02:26:27 +0800

    Create about.tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm Cornelius and I have so so many hobbies.</p>
+    </Layout>
+  )
+}
+export default AboutPage
