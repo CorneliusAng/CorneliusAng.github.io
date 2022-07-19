8d8044e557cd36d9b576a795c674aadb1d1b9330
mochidamnit <corneliusang@outlook.com>
2022-07-19 21:46:30 +0800

    Implement tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/layout";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>Hi, my name is Cornelius.</p>
+      <p className="text-3xl font-bold underline">Hi, my name is Cornelius.</p>
       <p>I am currently serving my National Service in the Republic of Singapore Air Force. I am currently coding a bunch of stuffs to boost my portfolio.</p>
     </Layout>
   )
