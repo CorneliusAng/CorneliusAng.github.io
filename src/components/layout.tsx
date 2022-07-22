8f85934b30c36788558f2007b8fd1a172ba25123
Cornelius Ang <corneliusang@outlook.com>
2022-07-23 04:29:09 +0800

    Add projects.tsx
    Remove unnecessary links in layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Navbar from '../components/navbar'
+
 interface layoutProps {
   pageTitle: String;
   children: React.ReactNode;
@@ -11,15 +11,9 @@ const Layout = ({ pageTitle, children }: layoutProps) => {
     <div>
       <Navbar/>
       <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
+
       <main>
-        <h1>{pageTitle}</h1>
-        {children}
+        <h1>{children}</h1>
       </main>
     </div>
   )
---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const ProjectPage = () => {
+  return (
+    <Layout pageTitle="Projects Page">
+      <p className="text-3xl font-bold underline">These are stuff that I found out</p>
+      <p>navbarContent[] interface did not work on the website but worked in VSC. I used Object.values() instead which solves the problem.</p>
+    </Layout>
+  )
+}
+export default ProjectPage
